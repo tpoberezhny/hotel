b402658f91c5ab11b0bb030527015d6f00283616
Tim <poberezhny003@gmail.com>
2023-06-11 22:35:53 +0200

    Spinners React was added on Check Now btn

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -4,11 +4,18 @@ import { RoomContext } from "../context/RoomContext";
 
 import Room from "../components/Room";
 
+import { SpinnerDotted } from "spinners-react";
+
 function Rooms() {
-  const { rooms } = useContext(RoomContext);
+  const { rooms, loading } = useContext(RoomContext);
 
   return (
     <div className="py-24">
+      {loading && (
+        <div className="h-screen fixed bottom-0 top-0 bg-black/90 w-full z-50 flex justify-center items-cemter">
+          <SpinnerDotted thickness={100} color="rgba(163, 125, 50, 1)"/>
+        </div>
+      )}
       <div className="container mx-auto lg:px-0">
         <div className="grid grid-cols-1 max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0">
           {rooms.map((room) => {
--- src/context/RoomContext.js
+++ src/context/RoomContext.js
@@ -9,32 +9,49 @@ function RoomProvider({ children }) {
   const [kids, setKids] = useState("0 Kids");
   const [total, setTotal] = useState(0);
   const [rooms, setRooms] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setTotal(Number(adults[0]) + Number(kids[0]));
   });
 
-   const handleClick = (e) => {
-      e.preventDefault();
-      //filter rooms based on total (person)
-      const newRooms = globalRooms.filter((room) => {
-        return total <= room.maxPerson;
-      });
+  const handleClick = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    //filter rooms based on total (person)
+    const newRooms = globalRooms.filter((room) => {
+      return total <= room.maxPerson;
+    });
+    setTimeout(() => {
       setRooms(newRooms);
-    }
+      setLoading(false);
+    }, 1000);
+  };
 
   useEffect(() => {
     axios
       .get("https://647bbfa9c0bae2880ad03a9e.mockapi.io/rooms")
       .then((res) => {
         setGlobalRooms(res.data);
-        setRooms(res.data)
+        setRooms(res.data);
       });
   }, []);
 
   return (
     <RoomContext.Provider
-      value={{ globalRooms, setGlobalRooms, adults, setAdults, kids, setKids, handleClick, rooms, setRooms }}
+      value={{
+        globalRooms,
+        setGlobalRooms,
+        adults,
+        setAdults,
+        kids,
+        setKids,
+        handleClick,
+        rooms,
+        setRooms,
+        loading,
+        setLoading,
+      }}
     >
       {children}
     </RoomContext.Provider>
